9c6c7e886aa937666d8ea6f2672c48964231d9a6
rigeljs <rigeljs@gmail.com>
2014-04-09 12:32:59 -0400

    added comments, changed variable names

--- project/make_pairsWithFeatures.py
+++ project/make_pairsWithFeatures.py
@@ -11,13 +11,15 @@ result = open("pairs_withFeatures.txt", "w")
 
 onegram_file = open(opts.one_grams, "r")
 
-one_grams_e_f = {}
+one_grams_f_e = {}
 for line in onegram_file:
 	f, e, feats = (l.strip() for l in line.split("|||"))
 	prob, _, _, _ = (float(n.strip()) for n in feats.split(" "))
-	if f not in one_grams_e_f:
-		one_grams_e_f[f] = {}
-	one_grams_e_f[f][e] = prob
+	#the value at one_grams_f_e[f] for each f is a mapping from the english translation
+	#to its probability
+	if f not in one_grams_f_e:
+		one_grams_f_e[f] = {}
+	one_grams_f_e[f][e] = prob
 
 
 e_sentences = open(opts.english_pairs, "r")
@@ -46,12 +48,12 @@ while e_line and f_line:
 			#find num of spanish translation found in english
 			totalProb = 0
 			for f_w in f_words:
-				if f_w in one_grams_e_f:
+				if f_w in one_grams_f_e:
 					#find english translations of f_w that are present in the english sentences
-					matches = (set(one_grams_e_f[f_w].keys()) & set(e_words))
+					matches = (set(one_grams_f_e[f_w].keys()) & set(e_words))
 					if len(matches) > 0:
 						#add the probability from the best match
-						totalProb += max([one_grams_e_f[f_w][e] for e in matches])
+						totalProb += max([one_grams_f_e[f_w][e] for e in matches])
 			translationScore = float(totalProb) / len(f_words)
 			result.write(e_sent + " ||| " + f_sent)
 			result.write(" ||| " + str(length_diff) + " " + str(translationScore) + "\n")
