b097abab290b522a68e49b258c408ce68663ebda
nikkil331 <limni@seas.upenn.edu>
2014-04-09 19:39:41 -0400

    weights are all 0

--- project/learn_weights.py
+++ project/learn_weights.py
@@ -1,27 +1,33 @@
 import optparse
 import mlpy
+import numpy as np
 
 optparser = optparse.OptionParser()
-optparser.add_option("-f", "--file", dest="fileName", default="pairs_withFeatures.txt", help="Each pair of sentences with pair features")
-optparser.add_option("-ep", "--english_pairs", dest="english_pairs", default="data/pairs.enu.dev", help="english pairs")
-optparser.add_option("-fp", "--foreign_pairs", dest="foreign_pairs", default="data/pairs.esn.dev", help="foreign pairs")
+optparser.add_option("-n", "--file", dest="fileName", default="pairs_withFeatures.txt", help="Each pair of sentences with pair features")
+optparser.add_option("-e", "--english_pairs", dest="english_pairs", default="data/pairs.enu.dev", help="english pairs")
+optparser.add_option("-f", "--foreign_pairs", dest="foreign_pairs", default="data/pairs.esn.dev", help="foreign pairs")
+(opts, args) = optparser.parse_args()
 
-featData = []
-classifications = []
-truePairings = zip(open(optparser.english_pairs, "r"), open(optparser.foreign_pairs, "r"))
+paired_features = []
+notpaired_features = []
+paired = []
+notpaired = []
+truePairings = [(e.strip(), f.strip()) for (e,f) in zip(open(opts.english_pairs, "r"), open(opts.foreign_pairs, "r"))]
 
-with open(optparser.fileName, "r") as f:
-	for line in f:
-		(e, f, features) = f.split("|||")
-		features = features.split(" ")
-		featData.append(features)
-		if (e,f in truePairings):
-			classifications.append(1)
+with open(opts.fileName, "r") as pairs:
+	for line in pairs:
+		(e, f, features) = line.split("|||")
+		features = [float(feat) for feat in features.strip().split(" ")]
+		if ((e.strip(),f.strip()) in truePairings):
+			paired.append(1)
+			paired_features.append(features)
 		else:
-			classifications.append(-1)
+			notpaired.append(-1)
+			notpaired_features.append(features)
+
 
 en = mlpy.ElasticNetC(lmb=0.01, eps=0.0001)
-en.learn(featData, classifications)
+en.learn(np.concatenate((paired_features, notpaired_features), axis=0), np.concatenate((paired, notpaired)))
 w = en.w()
 
 print " ".join([str(weight) for weight in w])
